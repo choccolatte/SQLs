332151a472b0da4d92ac232f54bd831d3432cf3a
A Sin <124335907+choccolatte@users.noreply.github.com>
2025-11-20 01:06:15 +0530

    more sql query eg. -2011250106

--- commands.sql
+++ commands.sql
@@ -3,3 +3,13 @@ SELECT * FROM "demodata.csv" WHERE 'turnover' IS NOT NULL LIMIT 10;
 SELECT "title", "author" FROM "demodata.csv" WHERE 'gender' = 'MALE';
 
 SELECT "title", "rating" FROM "demodata.csv" ORDER BY "rating" DESC LIMIT 10;
+
+SELECT "title", "rating", "votes" FROM "demodata.csv" ORDER BY "rating" DESC, "votes" DESC LIMIT 10;
+
+SELECT AVG("rating") FROM "longlist";
+
+SELECT ROUND(AVG("rating"), 2) FROM "longlist";
+
+SELECT ROUND(AVG("rating"), 2) AS "average rating" FROM "longlist";
+
+SELECT COUNT(DISTINCT "publisher") FROM "longlist";
