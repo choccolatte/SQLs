231c09a81d9b9af472aeda98f1a70c9783262e4e
A Sin <124335907+choccolatte@users.noreply.github.com>
2025-11-19 23:48:23 +0530

    sql query examples - v1911252348

--- commands.sql
+++ commands.sql
@@ -1,1 +1,5 @@
 SELECT * FROM "demodata.csv" WHERE 'turnover' IS NOT NULL LIMIT 10;
+
+SELECT "title", "author" FROM "demodata.csv" WHERE 'gender' = 'MALE';
+
+SELECT "title", "rating" FROM "demodata.csv" ORDER BY "rating" DESC LIMIT 10;
